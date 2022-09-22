ce35cb644df523f530b4802e3fc3d19e35f0b6ed
Jayawinath Induwara <90089540+induwara43@users.noreply.github.com>
2022-09-22 15:03:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+function textbold() {
+    document.execCommand("bold");
+}
+function textunderline() {
+    document.execCommand("underline");
+}
+function textitalic() {
+    document.execCommand("italic");
+}
+function textcolor() {
+    let pickedcolor = document.getElementById("colorpick").value
+    document.execCommand("forecolor", true, pickedcolor);
+}
+function textleft() {
+    document.execCommand("Justifyleft");
+}
+function textcenter() {
+    document.execCommand("JustifyCenter");
+}
+function textright() {
+    document.execCommand("JustifyRight");
+}
+function textjustify() {
+    document.execCommand("JustifyFull");
+}
+function redo() {
+    document.execCommand("redo");
+}
+function undo() {
+    document.execCommand("undo");
+}
